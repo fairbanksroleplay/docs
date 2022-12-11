b6bc73b600cefbb3fc3242fe08dcb925d20411e0
fairbanksroleplay <120164479+fairbanksroleplay@users.noreply.github.com>
2022-12-11 22:48:31 +1300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="docs"
+            href="/docs"
           >
             Get Started
           </Link>
