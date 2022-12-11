6994d85633eb36cc3ee734bb0c3f39d94edb6a23
fairbanksroleplay <120164479+fairbanksroleplay@users.noreply.github.com>
2022-12-11 22:37:28 +1300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            href="https://fbrp.xyz/discord"
+            to="/docs"
           >
             Get Started
           </Link>
